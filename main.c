d7d7cf44b817c5f1eb2b91dc1241bdb77592f7bd
chenwa234 <44318573+chenwa234@users.noreply.github.com>
2019-11-04 18:28:13 -0500

    Add files via upload
    MD5 with that determines max length

--- main.c
+++ main.c
@@ -1,40 +1,86 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <stdint.h>
+
+// leftrotate function definition
+#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+
+// These vars will contain the hash
+
+uint32_t h0, h1, h2, h3;
+void md5(uint8_t *initial_msg, size_t initial_len);
 
 
 int main()
 {
-    int test[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int file_counter = 9;
-    int stores[10000];
-    int slot = 0, i;
-
-    while(file_counter != 1) {
-        slot = 0;
-        for(i = 0; i <= file_counter - 1; i++) {
-            printf("%d\t", test[i]);
-        }
-        puts("");
-
-        for(i = 0; i <= file_counter - 2; i+=2) {
-            test[slot] = test[i] + test[i+1];
-            slot++;
-        }
-
-        if(file_counter % 2 == 1) {
-            test[slot] = test[file_counter-1];
-        }
-
-        test[slot] = test[file_counter-1];
-        for(i = slot + 1; i <= file_counter - 1; i++) {
-            //s[i] = NULL;
-        }
-        file_counter = file_counter/2 + file_counter%2;
-
-    }
-
-
-
 
     return 0;
+
 }
+
+
+void md5(uint8_t *initial_msg, size_t initial_len)
+{
+    // some variables
+    int i;
+    int max_len = 448;
+
+    // Message (to prepare)
+
+    uint8_t *msg = NULL;
+
+    // Note: All variables are unsigned 32 bit and wrap modulo 2^32 when calculating
+    // r specifies the per-round shift amounts
+
+    uint32_t s[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+
+                    5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+
+                    4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+
+                    6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+                   };
+
+    // Use binary integer part of the sines of integers (in radians) as constants// Initialize variables:
+
+    uint32_t k[] =
+    {
+
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+    };
+
+    h0 = 0x67452301; // A
+    h1 = 0xefcdab89; // B
+    h2 = 0x98badcfe; // C
+    h3 = 0x10325476; // D
+
+    // Note initial len is in length
+    uint32_t new_length = (initial_len * 8) - 8; // get number of bits used excluding final'\n
+
+    while (new_length > max_len){
+
+        max_len += 512;
+    }
+
+    new_length = max_len; // now we know the length of the string
+
+}
+
